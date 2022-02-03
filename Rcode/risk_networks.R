b6881329512233b9b88a25e8cc8932d8c9ae9b3b
ElenaVillano <villaele14@gmail.com>
2022-02-02 19:07:12 -0600

    update en codigo e imagenes

--- Rcode/risk_networks.R
+++ Rcode/risk_networks.R
@@ -1,4 +1,5 @@
-############## Análisis con los datos del tamizaje ###############
+# Network analysis for Screening
+# By Elena Villalobos, Enero 2022
 
 # limpiamos escritorio
 rm(list=ls())
@@ -12,11 +13,16 @@ library("bootnet")
 # fija tu directorio de trabajo en una carpeta específica
 setwd("~/Documents/mental_networks")
 
-# Base de datos
-datos_raw_riesgo <- read_csv('data/BDCOVIDNivelRiesgo.csv')
+## Base de datos
+#datos_raw_riesgo <- read_csv('data/complete_data/BDCOVIDNivelRiesgo.csv')
+## Los datos completos están en la base de BDCOVID
+## las preguntas están en el csv de preguntas.csv
+## y las preguntas de sólo las psicopatologías son las siguientes
+#respuestas <- datos_raw_riesgo[,42:73]
+#write_csv(respuestas, 'data/risk_ans.csv')
 
-# preguntas de sólo la psicopatologías
-respuestas <- datos_raw_riesgo[,42:74]
+## Data
+respuestas <- read_csv('data/risk_ans.csv')
 
 # renombramos para facilidad de interpretacion
 names(respuestas) <- c('st1','st2','st3','st4','av5',
@@ -25,13 +31,12 @@ names(respuestas) <- c('st1','st2','st3','st4','av5',
                        'di16','st17','aG18','aG19','aG20',
                        'aG21','aG22','aS23','aS24','aS25',
                        'so26','so27','so28','so29','so30',
-                       'de31','de32','de33')
+                       'de31','de32')
 
 # 1 conjunto de preguntas
 pcl5_set <- respuestas[,1:17]
-dean_set <- respuestas[,18:33]
+dean_set <- respuestas[,18:32]
 todo_set <- respuestas[,1:32]
-
 
 
 # Estimate network:
@@ -52,23 +57,29 @@ net_todo <- todo_net$graph
 ########## Network
 # Estimated network:
 qgraph(net_pcl5, 
-       layout='spring',filename='Rcode/nets/net_pcl5', filetype = "pdf", 
+       layout='spring',filename='documentation/images/net_pcl5', filetype = "pdf", 
        height = 8, width = 8)
 
 qgraph(net_dean, 
-       layout='spring',filename='Rcode/nets/net_dean', filetype = "pdf", 
+       layout='spring',filename='documentation/images/net_dean', filetype = "pdf", 
        height = 8, width = 8)
 
 qgraph(net_todo, 
-       layout='spring',filename='Rcode/nets/net_todo', filetype = "pdf", 
+       layout='spring',filename='documentation/images/net_todo', filetype = "pdf", 
        height = 8, width = 8)
 
 # Medidas de centralidad para la red
-pdf(file='Rcode/nets/centrality_nets.pdf', width = 8, height = 8, onefile = T)
+pdf(file='documentation/images/centrality_net_pcl5.pdf', width = 8, height = 8)#, onefile = T)
 centralityPlot(pcl5_net, include = c("Betweenness", "Closeness", "Strength"),
                orderBy = 'Strength')
+dev.off()
+
+pdf(file='documentation/images/centrality_net_dean.pdf', width = 8, height = 8)
 centralityPlot(dean_net, include = c("Betweenness", "Closeness", "Strength"),
                orderBy = 'Strength')
+dev.off()
+
+pdf(file='documentation/images/centrality_net_todo.pdf', width = 8, height = 8)
 centralityPlot(todo_net, include = c("Betweenness", "Closeness", "Strength"),
                orderBy = 'Strength')
 dev.off()
@@ -85,12 +96,17 @@ summary(boot_IC_dean)
 summary(boot_IC_todo)
 
 # Plot bootstrapped edge CIs:
-pdf(file='Rcode/nets/interval_boots.pdf', width = 10, height = 8, onefile = T)
+pdf(file='documentation/images/interval_pcl15.pdf', width = 10, height = 8, onefile = T)
 plot(boot_IC_pcl5, labels = FALSE, order = "sample")
-plot(boot_IC_dean, labels = FALSE, order = "sample")
-plot(boot_IC_todo, labels = FALSE, order = "sample")
 dev.off()
 
+pdf(file='documentation/images/interval_dean.pdf', width = 10, height = 8, onefile = T)
+plot(boot_IC_dean, labels = FALSE, order = "sample")
+dev.off()
+
+pdf(file='documentation/images/interval_todos.pdf', width = 10, height = 8, onefile = T)
+plot(boot_IC_todo, labels = FALSE, order = "sample")
+dev.off()
 ######### B. Bootstrap de subconjuntos para ver la estabilidad de las medidas de centralidad
 
 boot_cen_pcl5 <- bootnet(pcl5_net, nBoots = 1000, type = 'case',
@@ -100,9 +116,15 @@ boot_cen_dean <- bootnet(dean_net, nBoots = 1000, type = 'case',
 boot_cen_todo <- bootnet(todo_net, nBoots = 1000, type = 'case',
                          statistics= c("strength", "closeness","betweenness"))
 
-pdf(file='Rcode/nets/estability.pdf', width = 10, height = 8, onefile = T)
+pdf(file='documentation/images/estability.pdf', width = 10, height = 8, onefile = T)
 plot(boot_cen_pcl5, statistics = c("strength", "closeness","betweenness"))
+dev.off()
+
+pdf(file='documentation/images/estabilit_pcl5.pdf', width = 10, height = 8, onefile = T)
 plot(boot_cen_dean, statistics = c("strength", "closeness","betweenness"))
+dev.off()
+
+pdf(file='documentation/images/estability_pcl5.pdf', width = 10, height = 8, onefile = T)
 plot(boot_cen_todo, statistics = c("strength", "closeness","betweenness"))
 dev.off()
 
@@ -114,16 +136,27 @@ corStability(boot_cen_todo)
 
 differenceTest(boot_IC_pcl5, 3, 17,'strength')
 
-pdf(file='Rcode/nets/signifi.pdf', width = 9, height = 9, onefile = T)
+pdf(file='documentation/images/signif_pcl5.pdf', width = 9, height = 9, onefile = T)
 plot(boot_IC_pcl5, 'edge', plot = "difference", onlyNonZero = TRUE, order='sample')
+dev.off()
+
+pdf(file='documentation/images/signifi_dean.pdf', width = 9, height = 9, onefile = T)
 plot(boot_IC_dean, 'edge', plot = "difference", onlyNonZero = TRUE, order='sample')
+dev.off()
+
+pdf(file='documentation/images/signifi_todo.pdf', width = 9, height = 9, onefile = T)
 plot(boot_IC_todo, 'edge', plot = "difference", onlyNonZero = TRUE, order='sample')
 dev.off()
 
-pdf(file='Rcode/nets/signif_2i.pdf', width = 9, height = 9, onefile = T)
-
+pdf(file='documentation/images/signif_2i.pdf', width = 9, height = 9, onefile = T)
 plot(boot_IC_pcl5, "strength")
+dev.off()
+
+pdf(file='documentation/images/signif_2i.pdf', width = 9, height = 9, onefile = T)
 plot(boot_IC_dean, "strength")
+dev.off()
+
+pdf(file='documentation/images/signif_2i.pdf', width = 9, height = 9, onefile = T)
 plot(boot_IC_todo, "strength")
 dev.off()
 
